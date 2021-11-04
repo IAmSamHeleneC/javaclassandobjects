b5787ec1501a57afd11dd607ada3103efb511860
Samantha Helene C <91547802+IAmSamHeleneC@users.noreply.github.com>
2021-11-04 08:39:34 -0400

    Created pet.java

---
+++ pet.java
@@ -0,0 +1,35 @@
+public class Pet {
+    public static void main(String[]args) {
+        
+    // name (PRIVATE) 
+    // age (PRIVATE) 
+    // location (PRIVATE) 
+    // type (PRIVATE) 
+
+Pet new_pet = new Pet (); }
+    private new_pet.name Chipper;
+    private new_pet.age = 3;
+    private new_pet.location = London
+    
+    // two constructors(empty, all attributes)
+    public Pet(String first, String last);
+    {
+     firstName = first;
+     lastName = last;
+    }
+    public Pet(String first, String last);
+    {
+     firstName = first;
+     lastName = last;
+    }
+    // Code to be able to access the following (Get Methods): 
+
+    // name, age, type 
+
+    // Code to be able to change (Set Methods): 
+
+    // name, age, location    
+{   
+}
+   }
+
