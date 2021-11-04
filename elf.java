1d8a507b53317aa593f79f9eef15c9024b4e81d4
Samantha Helene C <91547802+IAmSamHeleneC@users.noreply.github.com>
2021-11-04 08:38:57 -0400

    Created elf.java

---
+++ elf.java
@@ -0,0 +1,33 @@
+public class Elf {
+    public static void main(String[] args){
+        Elf new_profile = new Elf ("Dasher", 22);
+        System.out.println(new_profile.name);  
+//     Minimum 3 private instance variables 
+{    
+Elf new_profile = new Elf (); }
+    private new_profile.name Dasher;
+    private new_profile.age = 22;
+    private new_profile.id = 78912;
+
+    // An empty constructor 
+    // A constructor that uses only 2 of your 3 variables
+    // A constructor that uses all 3 of your instance variables
+    public Elf (String name, Integer age) {
+        this.name = name;
+        this.age = age; 
+    }
+// A method that returns a Boolean 
+// A method that returns a String
+static Elf addElfs(String name, int age, boolean employed){
+   
+    Elf new_profile = new Elf();
+    new_student.name = name;
+    new_student.age = age;
+    new_student.id = idNumber;
+    return new_profile;
+
+    }
+ 
+// Get methods for all 3 of your instance variables and Set methods for all 3 of your instance variables 
+
+}
